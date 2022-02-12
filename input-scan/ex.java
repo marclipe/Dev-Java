397c8aed65de558669588a603c89571029a1868e
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-02-12 13:19:49 -0300

    The Begin

---
+++ input-scan/ex.java
@@ -0,0 +1,12 @@
+/**
+ * ex
+ */
+public class ex {
+    public static void main(String[] args) {
+        int num = 2; 
+        int num2 = 3; 
+        double sum;
+        sum = num + num2; 
+        System.out.println(sum);
+    }
+}
