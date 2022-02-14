026b54b8de8c6b6a9b9967afdbe01bb47a909e45
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-02-14 19:58:48 -0300

    If/Else, Conditions, Booleans

---
+++ Coditions and Booleans/ex.java
@@ -0,0 +1,14 @@
+public class ex {
+    public static void main(String[] args) {
+        int x = 6; 
+        int y = 23; 
+        int z = 10; 
+        /*String h = "hello";
+        String i = "hi"; */
+        // > < == >= <= !=
+        boolean compare = !(x > y || z < y); 
+        //boolean compare_string = h == i; 
+        System.out.println(compare);
+        //System.out.println(compare_string);
+    }
+}
---
+++ If-Else/ex.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+/**
+ * if
+ */
+public class ex  {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine(); 
+
+        if (s.equals("tim")) {
+            System.out.println("You typed");
+        } 
+        
+    }
+}
+
