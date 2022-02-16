09c87eecf54f96224c164fb260f90e5adec740ef
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-02-16 20:06:22 -0300

    Tipos primitivos <3

---
+++ DevDojo/ex001/src/acdemy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,7 @@
+package acdemy.devdojo.maratonajava.introducao;
+
+public class Aula01OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+    }
+}
---
+++ DevDojo/ex001/src/acdemy/devdojo/maratonajava/introducao/Aula2TiposPrimitivos.java
@@ -0,0 +1,19 @@
+package acdemy.devdojo.maratonajava.introducao;
+
+public class Aula2TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, lang, boolean
+        int age = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0041'; /*Vai mostrar o caractere da tabela Asscii e ovalor unicode*/
+        System.out.println("A idade é: " + age + " anos.");
+        System.out.println(verdadeiro);
+        System.out.println("char " + caractere);
+    }
+}
