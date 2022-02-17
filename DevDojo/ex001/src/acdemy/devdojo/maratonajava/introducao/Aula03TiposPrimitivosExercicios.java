3e4a102b0de3d5cd00e82ae87cf3fb6b4ccd216b
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-02-17 17:36:24 -0300

    Operadores Aritméticos

--- DevDojo/ex001/src/acdemy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicios.java
+++ DevDojo/ex001/src/acdemy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicios.java
@@ -4,7 +4,7 @@ public class Aula03TiposPrimitivosExercicios {
     public static void main(String[] args) {
         String nome = "Marcos";
         String endereco = "Rua José Ferreira, 53";
-        double salario = 1500;
+        double salario = 8500.15;
         String data = "30/03/2022";
         System.out.println("Eu " + nome + ", morando no endereço " + endereco + " confirmo que recebi o " + salario + ", na " + data);
     }
---
+++ DevDojo/ex001/src/acdemy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,16 @@
+package acdemy.devdojo.maratonajava.introducao;
+
+
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        int number01 = 10;
+        int number02 = 20;
+        int resultado = number01 + number02;
+        double result = number02 / number01;
+        System.out.println(number02 - number01);
+        System.out.println(resultado);
+        System.out.println(result);
+    }
+}
