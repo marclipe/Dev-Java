d4349248b19c55a53bc4ae336db45e4b1db99284
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-02-17 17:12:54 -0300

    Tipos Primitivos

---
+++ DevDojo/ex001/src/acdemy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicios.java
@@ -0,0 +1,11 @@
+package acdemy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicios {
+    public static void main(String[] args) {
+        String nome = "Marcos";
+        String endereco = "Rua José Ferreira, 53";
+        double salario = 1500;
+        String data = "30/03/2022";
+        System.out.println("Eu " + nome + ", morando no endereço " + endereco + " confirmo que recebi o " + salario + ", na " + data);
+    }
+}
--- DevDojo/ex001/src/acdemy/devdojo/maratonajava/introducao/Aula2TiposPrimitivos.java
+++ DevDojo/ex001/src/acdemy/devdojo/maratonajava/introducao/Aula2TiposPrimitivos.java
@@ -3,17 +3,20 @@ package acdemy.devdojo.maratonajava.introducao;
 public class Aula2TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, lang, boolean
-        int age = 10;
+        int age = 20;
         long numeroGrande = 100000;
         double salarioDouble = 2000;
-        float salarioFloat = 2500;
-        byte idadeByte = 10;
+        float salarioFloat = 2500.0F;
+        byte idadeByte = 127;
         short idadeShort = 10;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = '\u0041'; /*Vai mostrar o caractere da tabela Asscii e ovalor unicode*/
+        String nome = "MarcLipe"; //Aspas dupla
         System.out.println("A idade é: " + age + " anos.");
         System.out.println(verdadeiro);
         System.out.println("char " + caractere);
+        System.out.println(numeroGrande);
+        System.out.println("Oi meu nome é " + nome);
     }
 }
