e1179dfb692761baa1cdb0b34cce8350f1e997fb
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-02-16 22:27:18 -0300

    Trabalho

---
+++ trabalho/src/javatrabalho/matriz.java
@@ -0,0 +1,41 @@
+public class matriz {
+    public static void main(String[] args) {
+        String nome;
+        int idade;
+        char sexo;
+        double;
+        double array[0][0] = 1;
+        double array[0][1] = 2;
+        array[0][2] = 3;
+        array[1][0] = 4;
+        array[1][1] = 5;
+        array[1][2] = 6;
+        array[2][0] = 7;
+        array[2][1] = 8;
+        array[2][2] = 9;
+        System.out.print(array[0][0]);
+        System.out.print(array[0][1]);
+        System.out.print(array[0][2]);
+        System.out.println(&quot;&quot;);
+        System.out.print(array[1][0]);
+        System.out.print(array[1][1]);
+        System.out.print(array[1][2]);
+        System.out.println(&quot;&quot;);
+        System.out.print(array[2][0]);
+        System.out.print(array[2][1]);
+        System.out.print(array[2][2]);
+        System.out.println(&quot;&quot;);
+        System.out.println(&quot;&quot;);
+        System.out.println(&quot;&quot;);
+
+        for (int i = 0 ; array.length; i++){
+            for (int j = 0; array[i].length; j++){
+                System.out.print(array[i][j]);
+                System.out.println("");
+
+            }
+        }
+        System.out.println(&quot;&quot;);
+
+    }
+}
