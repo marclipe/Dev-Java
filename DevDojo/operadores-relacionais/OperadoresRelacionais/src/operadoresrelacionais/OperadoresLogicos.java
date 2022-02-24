9e73a3ef4b5e543c9bb887ff218b2122d40e17f5
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-02-24 17:39:10 -0300

    Ok

--- DevDojo/operadores-relacionais/OperadoresRelacionais/src/operadoresrelacionais/OperadoresLogicos.java
+++ DevDojo/operadores-relacionais/OperadoresRelacionais/src/operadoresrelacionais/OperadoresLogicos.java
@@ -23,5 +23,28 @@ public class OperadoresLogicos {
         boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario <= 3381; 
         System.out.println("isDentroDaLeiMaiorQueTrinta " + isDentroDaLeiMaiorQueTrinta);
         System.out.println("isDentroDaLeiMenorQueTrinta " + isDentroDaLeiMenorQueTrinta);
+        
+        double valorTotalContaCorrente = 200; 
+        double valorTotalContaPoupanca = 10000; 
+        float valorPlaystation = 5000F; 
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
+        System.out.println("isPlaystationCincoCompravel " + isPlaystationCincoCompravel);
+        
+        // = += -= *= %= 
+        double bonus = 1800; 
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2; 
+        bonus /= 2; 
+        bonus %= 2; 
+        System.out.println(bonus);
+        
+        int contador = 0; 
+        contador += 1; //contador = contador + 1
+        contador++; 
+        contador--; 
+        ++contador; 
+        --contador; 
+        System.out.println(contador);
     }
 }
