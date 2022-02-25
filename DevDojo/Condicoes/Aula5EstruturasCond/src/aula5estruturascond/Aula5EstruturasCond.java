021e307e9127107c882f1e039b1225b2b59c19a5
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-02-24 22:14:24 -0300

    Condicionais

---
+++ DevDojo/Condicoes/Aula5EstruturasCond/src/aula5estruturascond/Aula5EstruturasCond.java
@@ -0,0 +1,18 @@
+package aula5estruturascond;
+
+public class Aula5EstruturasCond {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int idade = 15;
+        boolean isAutorizadoComprarBebida = idade >= 18;
+        if (isAutorizadoComprarBebida) {
+            System.out.println("Autorizado a comprar bebida alcólica");
+        } else {
+            System.out.println("Não autorizado a comprar bebida alcólica");
+        }
+        
+        
+    }
+
+}
