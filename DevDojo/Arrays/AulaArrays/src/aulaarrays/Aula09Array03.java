8274f8eda98cdf004f5a125725c09f5b13df1f16
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-03-14 21:51:58 -0300

    Foreach

---
+++ DevDojo/Arrays/AulaArrays/src/aulaarrays/Aula09Array03.java
@@ -0,0 +1,19 @@
+package aulaarrays;
+
+public class Aula09Array03 {
+
+    public static void main(String[] args) {
+
+        String[] nomes = new String[4];
+        nomes[0] = "Goku";
+        nomes[1] = "Kurosaki";
+        nomes[2] = "Luffy";
+        nomes[3] = "Hinata"; 
+        
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+
+    }
+}
