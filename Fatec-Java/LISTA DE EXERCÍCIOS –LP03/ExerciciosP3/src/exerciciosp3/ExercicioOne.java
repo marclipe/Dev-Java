01248038a24ec1186978c78801d79c91897027ab
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-03-15 21:27:06 -0300

    Exercícios L3

---
+++ Fatec-Java/LISTA DE EXERCÍCIOS –LP03/ExerciciosP3/src/exerciciosp3/ExercicioOne.java
@@ -0,0 +1,33 @@
+package exerciciosp3;
+
+/*Um  programa  capaz  de  imprimir  todos  os  números  pares  em  um  intervalo de números informador pelo usuário;*/
+import javax.swing.JOptionPane;
+
+public class ExercicioOne {
+
+    static double inicio, fim;
+
+    public static void main(String[] args) {
+        String Q = ""; 
+        try {
+            String x = JOptionPane.showInputDialog("Digite o um inicio: ");
+            inicio = Double.parseDouble(x);
+            
+            String y = JOptionPane.showInputDialog("Digite o fim: "); 
+            fim = Double.parseDouble(y); 
+            
+
+            for (inicio = inicio; inicio <= fim; inicio++) {
+                if (inicio % 2 == 0) {
+                    Q = Q + String.valueOf(inicio + " "); 
+                }
+
+            }
+            JOptionPane.showMessageDialog(null, Q);
+
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "[ERRO] Digite apenas números!!");
+        }
+
+    }
+}
