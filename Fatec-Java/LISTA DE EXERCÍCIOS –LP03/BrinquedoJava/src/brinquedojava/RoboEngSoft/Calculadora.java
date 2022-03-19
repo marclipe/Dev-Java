c48ef6f8b56e0345a3a3264f72456a7a90299bea
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-03-19 18:21:14 -0300

    Try Catch

---
+++ Fatec-Java/LISTA DE EXERCÍCIOS –LP03/BrinquedoJava/src/brinquedojava/RoboEngSoft/Calculadora.java
@@ -0,0 +1,21 @@
+public class Calculadora {
+
+	private Robo robo;
+
+	public double Soma(double numero1, double numero2 double) {
+		return 0;
+	}
+
+	public double Subtrair(double numero1, double numero2 double) {
+		return 0;
+	}
+
+	public double Multiplicar(double numero1, double numero2 double) {
+		return 0;
+	}
+
+	public double Dividir(double numero1, double numero2 double) {
+		return 0;
+	}
+
+}
---
+++ Fatec-Java/LISTA DE EXERCÍCIOS –LP03/ExerciciosP3/src/exerciciosp3/ExercicioTwo.java
@@ -0,0 +1,33 @@
+package exerciciosp3;
+
+import javax.swing.JOptionPane;
+
+public class ExercicioTwo {
+
+    static double n; 
+
+    static long fibo(int n) {
+        if (n < 2) {
+            return n;
+        } else {
+            return fibo(n - 1) + fibo(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        String M = "";
+        try {
+            String A = JOptionPane.showInputDialog("Digite até qual termo deseja ir: ");
+            n = Double.parseDouble(A);
+
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "[ERRO] Digite apenas números!!");
+        } finally {
+            for (int i = 1; i <= n; i++) {
+                M = M + String.valueOf("(" + i + "):" + ExercicioTwo.fibo(i) + " ");
+            }
+
+            JOptionPane.showMessageDialog(null, M);
+        }
+    }
+}
