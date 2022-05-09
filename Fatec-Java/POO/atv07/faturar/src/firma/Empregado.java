552a8037fce0ffa2033dd0e90c2f15edafb4ae6b
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-05-08 22:21:18 -0300

    Java POO

--- Fatec-Java/POO/atv07/faturar/src/firma/Empregado.java
+++ Fatec-Java/POO/atv07/faturar/src/firma/Empregado.java
@@ -1,6 +1,20 @@
 package firma;
 
+import javax.swing.JOptionPane;
 
 public class Empregado {
     
+    public static void main(String[] args) {
+        String primeiroNome; 
+        String sobrenome; 
+        double salarioMensal;
+        
+        primeiroNome = JOptionPane.showInputDialog("Primeiro Nome: ");
+        sobrenome = JOptionPane.showInputDialog("Sobrenome: ");
+        String n1 = JOptionPane.showInputDialog("Salário Anual: ");
+        salarioMensal = Double.parseDouble(n1); 
+        
+        
+        
+    }
 }
---
+++ Fatec-Java/POO/atv07/faturar/src/firma/EmpregadoTeste.java
@@ -0,0 +1,64 @@
+package firma;
+
+public class EmpregadoTeste {
+
+    String primeiroNome;
+    String sobrenome;
+    double salarioMensal;
+    double salarioAnual;
+    double resultado;
+
+    public EmpregadoTeste(String primeiroNome, String sobrenome, double salarioMensal) {
+        this.primeiroNome = primeiroNome;
+        this.sobrenome = sobrenome;
+        if (salarioMensal < 0) {
+            this.salarioMensal = 0;
+        } else {
+            this.salarioMensal = salarioMensal;
+        }
+    }
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public Double getSalarioMensal() {
+        return salarioMensal;
+    }
+
+    public void setSalarioMensal(double salarioMensal) {
+        this.salarioMensal = salarioMensal;
+    }
+
+    public double getSalarioAnual() {
+        return salarioAnual;
+    }
+
+    public void setSalarioAnual(double salarioAnual) {
+        this.salarioAnual = salarioAnual;
+    }
+
+    public void Setvalores(String a, String b, double c, double d) {
+        primeiroNome = a;
+        sobrenome = b;
+        salarioMensal = c;
+        salarioAnual = d;
+    }
+
+    public double GetEmpregadoTeste() {
+        resultado = (10 / 100) + salarioAnual;
+        return resultado;
+    }
+}
